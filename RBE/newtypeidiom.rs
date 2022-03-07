5e51287db4fb26bf792a811d66214ce238bfdf75
Rohit Dandamudi <rohitdandamudi.1100@gmail.com>
2022-03-07 21:52:59 +0530

    add RBE code executed locally

---
+++ RBE/newtypeidiom.rs
@@ -0,0 +1,9 @@
+struct Years(i64);
+
+fn main() {
+    let years = Years(42);
+    let _years_as_primitive_1: i64 = years.0; // Tuple
+    let Years(years_as_primitive_2) = years; // Destructuring
+    
+    println!("{}", years_as_primitive_2);
+}
---
+++ RBE/structures.rs
@@ -0,0 +1,101 @@
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+// A unit struct
+struct Unit;
+
+// A tuple struct
+struct Pair(i32, f32);
+
+// A struct with two fields
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Structs can be reused as fields of another struct
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Rectangle {
+    // A rectangle can be specified by where the top left and bottom right
+    // corners are in space.
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn square(point: Point, width: f32) -> Rectangle {
+    let Point {x: top_left_x, y: top_left_y} = point;
+    let Point {x: bottom_right_x, y: bottom_right_y} = Point {x: top_left_x + width, y: top_left_y + width};
+
+    return Rectangle { 
+        top_left: Point {x: top_left_x, y: top_left_y},
+        bottom_right: Point {x: bottom_right_x, y: bottom_right_y},
+    }
+}
+
+fn rect_area(rect: Rectangle) -> f32 {
+
+    let Rectangle{top_left, bottom_right} = rect;
+
+    return (top_left.y - bottom_right.y ) * (bottom_right.x - top_left.x);
+}
+
+fn main() {
+    // Create struct with field init shorthand
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Print debug struct
+    println!("{:?}", peter);
+
+
+    // Instantiate a `Point`
+    let point: Point = Point { x: 10.3, y: 0.4 };
+
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    // Make a new point by using struct update syntax to use the fields of our
+    // other one
+    let bottom_right = Point { x: 5.2, ..point };
+
+    // `bottom_right.y` will be the same as `point.y` because we used that field
+    // from `point`
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    // Destructure the point using a `let` binding
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
+
+    // area of rect
+
+    println!("rect area: {}", rect_area(_rectangle));
+
+    let square = square(point, 5.0);
+
+    println!("square formed: {:?}", square);
+
+    // Instantiate a unit struct
+    let _unit = Unit;
+
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
+
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+}
