7d97403da04f165f4e97e116e58113c6c8c6458f
Rohit Dandamudi <rohitdandamudi.1100@gmail.com>
2022-03-12 23:09:10 +0530

    add error handling and tests

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,7 +1,10 @@
-use core::fmt;
-use std::io::BufRead;
+#[allow(dead_code)]
+
+use anyhow::{Context, Result};
 
 use clap::Parser;
+
+// type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -13,23 +16,38 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-// impl fmt::Display for std::path::PathBuf {
-//     fn fmt(&self, f: &fmt::Formatter) -> fmt::Result {
-//         write!(f, "{}", self.0)
-//     }
-// }
-fn main() {
+
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    println!("{} {:?}", args.pattern, args.path);
-    let file = std::fs::File::open(&args.path).expect("wrong file path");
+    let content = std::fs::read_to_string(&args.path).expect("Couldn't read this file");
+    
+    find_matches(&content, &args.pattern, std::io::stdout()).with_context(|| "error finding matches")?;
 
-    let reader = std::io::BufReader::new(file);
+    Ok(())
+}
 
-    for line in reader.lines() {
-        let line = line.expect("not able to read line");
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line).with_context(|| "error writing mathces")?;
+            // return Err(anyhow::Error::msg("error writing mathces"));
         }
     }
+    Ok(())
+}
+
+
+
+#[test]
+fn find_a_match() {
+    let mut writer = Vec::new();
+    let result = find_matches("ipsum\ndolor sit amet", "lorem", &mut writer).with_context(|| "error writing matches");
+    match result {
+       
+        Ok(_) =>  assert_eq!(writer, b"lorem ipsum\n"),
+        Err(err) => assert_eq!("error writing matches", err.to_string()),
+    }
+    
+
 }
